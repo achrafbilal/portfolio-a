a117d2455fb58b2ffaadcdef3f624896cbba1b68
Achraf Bilal <ach.20raf@outlook.fr>
2021-09-11 11:41:42 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./css/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">Hello...</div>
+    </div>
+  );
+}
+
+export default App;
